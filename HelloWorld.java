0280820e3be9f6544c6c55214d90056a8e698cc7
EC2 Default User <ec2-user@ip-172-31-12-132.ap-south-1.compute.internal>
2022-12-28 15:55:55 +0000

    added new commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,13 +3,7 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
+    System.out.println("DevOps");
+    System.out.println("Jenkns job");
   }
 }
